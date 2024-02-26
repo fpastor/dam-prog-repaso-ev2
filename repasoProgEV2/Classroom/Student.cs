b6602f6280f0583b9032afb19d6fcd638a6a789b
Fernando Pastor <fpastor@icloud.com>
2024-02-26 12:32:54 +0100

    Update Student.cs

--- repasoProgEV2/Classroom/Student.cs
+++ repasoProgEV2/Classroom/Student.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace repasoProgEV2.Classroom
 {
     public class Student
