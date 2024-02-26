254e14384910dabe92fe413199f7e8feb774dd9b
Fernando Pastor <fpastor@icloud.com>
2024-02-26 12:32:43 +0100

    Create repasoVariosResultados.cs

---
+++ repasoProgEV2/repasoVariosResultados.cs
@@ -0,0 +1,22 @@
+﻿
+namespace repasoProgEV2
+{
+    internal class repasoVariosResultados
+    {
+        public static bool Calculate(int a, int b, out int result1, out int result2, out int result3, out int result4)
+        {
+            result1 = a + b;
+            result2 = a - b;
+            result3 = a * b;
+            result4 = 0;
+
+            if (b != 0) {
+                result4 = a / b;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
