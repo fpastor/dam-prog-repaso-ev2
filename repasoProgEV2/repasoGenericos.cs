c2ab5d077e00bfb94fc58089774fad49f8b0270a
Fernando Pastor <fpastor@icloud.com>
2024-02-26 12:32:45 +0100

    Create repasoGenericos.cs

---
+++ repasoProgEV2/repasoGenericos.cs
@@ -0,0 +1,14 @@
+﻿
+namespace repasoProgEV2
+{
+    public class repasoGenericos
+    {
+        class Generico<T>
+        {
+            public double Id;
+            public string Name = string.Empty;
+            public List<T> Data = new();
+        }
+
+    }
+}
