ae492293b3e49ed23ef783a09d5b32bcaf9004ae
Fernando Pastor <fpastor@icloud.com>
2024-02-26 12:32:46 +0100

    Create repasoExcepciones.cs

---
+++ repasoProgEV2/repasoExcepciones.cs
@@ -0,0 +1,24 @@
+﻿
+namespace repasoProgEV2
+{
+    public class repasoExcepciones
+    {
+
+        public void respasoExcepcion(int value1, int value2)
+        {
+            try
+            {
+                repasoDelegados.Calc(value1, value2, (value1, value2) => value1 + value2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Fin");
+            }
+        }
+    }
+}
+
