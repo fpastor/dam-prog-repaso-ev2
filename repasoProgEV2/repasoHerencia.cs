fd4fe63e82f2ab85e3bedcfcaf6257af5d0c43d7
Fernando Pastor <fpastor@icloud.com>
2024-02-28 01:19:51 +0100

    Create repasoHerencia.cs

---
+++ repasoProgEV2/repasoHerencia.cs
@@ -0,0 +1,35 @@
+﻿
+namespace repasoProgEV2
+{
+    public class User
+    {
+        public int id;
+        public string name = string.Empty;
+        public string email = string.Empty;
+        public bool superuser;
+
+        public User()
+        {
+            superuser = false;
+        }
+    }
+
+
+    public class Student: User
+    {
+        public Student() : base()
+        {
+        }
+    }
+
+    public class Teacher : User
+    {
+        public Teacher() : base()
+        {
+            superuser = true;
+        }
+    }
+
+
+
+}
